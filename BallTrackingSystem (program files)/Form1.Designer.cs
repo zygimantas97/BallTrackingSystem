236628263af61ab4e26919afdd0d89510551dfc2
zygimantas97 <zygimantas1997@gmail.com>
2019-02-27 03:04:21 +0200

    Pradeta programa
    sukurta windows forma;
    Įgyvendintas vaizdo įkėlimas ir konvertavimas bei filtravimas (reikia istestuoti);
    Reikia pabaigti implementuoti papildomas funkcijas

---
+++ BallTrackingSystem (program files)/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace BallTrackingSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            this.imageBox3 = new Emgu.CV.UI.ImageBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.imageBox3);
+            this.panel1.Controls.Add(this.imageBox2);
+            this.panel1.Controls.Add(this.imageBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.linkLabel1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 431);
+            this.panel1.TabIndex = 0;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Enabled = false;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.linkLabel1.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
+            this.linkLabel1.Location = new System.Drawing.Point(16, 10);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(255, 31);
+            this.linkLabel1.TabIndex = 0;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "BallTrackingSystem";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(290, 18);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(22, 75);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(210, 160);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.Location = new System.Drawing.Point(249, 75);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(210, 160);
+            this.imageBox2.TabIndex = 2;
+            this.imageBox2.TabStop = false;
+            // 
+            // imageBox3
+            // 
+            this.imageBox3.Location = new System.Drawing.Point(478, 75);
+            this.imageBox3.Name = "imageBox3";
+            this.imageBox3.Size = new System.Drawing.Size(210, 160);
+            this.imageBox3.TabIndex = 2;
+            this.imageBox3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(746, 298);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Button button1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private Emgu.CV.UI.ImageBox imageBox3;
+        private Emgu.CV.UI.ImageBox imageBox2;
+        private Emgu.CV.UI.ImageBox imageBox1;
+    }
+}
+
---
+++ BallTrackingSystem (program files)/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace BallTrackingSystem
+{
+    public partial class Form1 : Form
+    {
+        // initial min and max HSV filter values
+        int H_MIN = 0;
+        int H_MAX = 256;
+        int S_MIN = 0;
+        int S_MAX = 256;
+        int V_MIN = 0;
+        int V_MAX = 256;
+
+        //capture width and hight
+        const int FRAME_WIDTH = 640;
+        const int FRAME_HEIGHT = 480;
+        
+        // max number of objects to be detected
+        const int MAX_NUM_OBJECTS = 10;
+
+        // minimum and maximum object area
+        const int MIN_OBJECT_AREA = 20 * 20;
+        const int MAX_OBJECT_AREA = (int)((double)FRAME_HEIGHT * FRAME_WIDTH / 1.5);
+
+        // names of windows
+        const string windowName = "Original image";
+        const string windowName1 = "HSV image";
+        const string windowName2 = "Thresholded image";
+        const string windowName3 = "After morphological operations";
+        const string trackBarWindowName = "Trackbars";
+        static VideoCapture capture;
+
+        // status of inifinitive process
+        bool RUNNING = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool trackObjects = false;
+            bool useMorphOps = false;
+
+            // matrix to store each frame of the webcam feed
+            Mat cameraFeed = new Mat();
+            // matrix storage for HSV image
+            Mat HSV = new Mat();
+            // matrix storage for binary threshold image
+            Mat treshold = new Mat();
+            // x and y values for the location of the object
+            int x = 0;
+            int y = 0;
+            //create slider bars for HSV filtering
+            createTrackBars();
+            // video capture object to acquire webcam feed
+            VideoCapture capture = new VideoCapture(0);
+            // set weight width and height of capture frame
+            capture.SetCaptureProperty(Emgu.CV.CvEnum.CapProp.FrameWidth, FRAME_WIDTH);
+            capture.SetCaptureProperty(Emgu.CV.CvEnum.CapProp.FrameHeight, FRAME_HEIGHT);
+            while (RUNNING)
+            {
+                // store image to matrix
+                capture.Read(cameraFeed);
+                // convert frame from RGB to HSV colorspace
+                CvInvoke.CvtColor(cameraFeed, HSV, Emgu.CV.CvEnum.ColorConversion.Bgr2Hsv);
+                // filter HSV image between values
+                InputArray low = new InputArray(H_MIN, S_MIN, V_MIN);
+                MCvScalar up = new MCvScalar(H_MAX, S_MAX, V_MAX);
+                CvInvoke.InRange(HSV, new ScalarArray(new MCvScalar(H_MIN, S_MIN, V_MIN)), new ScalarArray(new MCvScalar(H_MAX, S_MAX, V_MAX)), treshold);
+
+                // eliminate noise
+                if (useMorphOps)
+                {
+                    MorphOps(out treshold);
+                }
+                // get coordinates of tracking object
+                if (trackObjects)
+                {
+                    trackFilteredObject(out x, out y, treshold, out cameraFeed);
+                }
+                //show frames
+                imageBox1.Image = cameraFeed.ToImage<Rgb, byte>();
+                imageBox2.Image = cameraFeed.ToImage<Hsv, byte>();
+                
+                
+            }
+            
+
+
+        }
+
+        private void MorphOps(out Mat treshold)
+        {
+
+        }
+        private void trackFilteredObject(out int x, out int y, Mat threshold, out Mat cameraFeed)
+        {
+
+        }
+
+
+    }
+}
